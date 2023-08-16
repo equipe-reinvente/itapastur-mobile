9527624b1414198fdc877c7c005734f66ac7a715
Matthews Bastos <matthewsbasttos@gmail.com>
2023-08-16 10:32:04 -0300

    fix: Imports createNativeStackNavigator to use it in the routes

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Login from "./src/views/Login";
 import Welcome from "./src/views/Welcome";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
