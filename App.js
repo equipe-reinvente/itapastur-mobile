d5aade19d0bf33e546527267123df64de63d63f3
Matthews Bastos <matthewsbasttos@gmail.com>
2023-08-16 10:20:02 -0300

    feat: Implements the navigation system in the App file

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
-import Login from './src/views/Login';
-import Welcome from './src/views/Welcome'
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+import Login from "./src/views/Login";
+import Welcome from "./src/views/Welcome";
+
 export default function App() {
   return (
-    <Welcome/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{ title: "Welcome" }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ title: "Login" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
