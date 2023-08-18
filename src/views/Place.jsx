5206bef2eb2de0cc244bbdc1175303f9e9a4970d
Matthews Bastos <matthewsbasttos@gmail.com>
2023-08-18 15:21:35 -0300

    refactor: Creates RouteTraceButton component and adds is into Place view

--- src/views/Place.jsx
+++ src/views/Place.jsx
@@ -1,11 +1,9 @@
 import { View, StyleSheet } from "react-native";
-import { Button, Text } from "@react-native-material/core";
-import { IconButton } from "@react-native-material/core";
-import { MaterialCommunityIcons } from '@expo/vector-icons';
 import PlaceTitle from "../components/PlaceTitle";
 import PlaceDescription from "../components/PlaceDescription";
 import ImageCarousel from "../components/ImageCarousel";
 import Socials from "../components/Socials";
+import RouteTraceButton from "../components/RouteTraceButton";
 
 const PlaceView = () => {
   const description = `A pedra do frade é cercada por lendas. Conta-se por exemplo,
@@ -36,25 +34,9 @@ const PlaceView = () => {
 
       <Socials onFavorite={() => {}} onShare={() => {}} />
 
-      <View style={styles.routeButtonContainer}>
-        <Button
-          title={"TRAÇAR ROTA"}
-          trailing={() => (
-            <MaterialCommunityIcons
-              name={'car'}
-              size={30}
-              color={"#FFFFFF"}
-            />
-          )}
-          style={styles.routeButton}
-          titleStyle={styles.routeButtonText}
-          color="#1daf6e"
-          contentContainerStyle={{height: 50}}
-          onPress={() => {}}
-        />
-      </View>
+      <RouteTraceButton onPress={() => {}}/>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -67,16 +49,6 @@ const styles = StyleSheet.create({
     height: 180,
     marginBottom: 90
   },
-  routeButtonContainer: {
-    marginTop: 100
-  },
-  routeButton: {
-    width: 350,
-  },
-  routeButtonText: {
-    color: "#FFFFFF",
-    fontSize: 16
-  }
 });
 
 export default PlaceView;
