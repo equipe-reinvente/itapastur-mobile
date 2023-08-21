6fec4990290854431c5f3e534c96249d3dd95434
Matthews Bastos <matthewsbasttos@gmail.com>
2023-08-21 18:22:51 -0300

    style: Adds some styles to CreateEnterprise

--- src/views/CreateEnterprise.jsx
+++ src/views/CreateEnterprise.jsx
@@ -37,9 +37,11 @@ const CreateEnterprise = () => {
   };
 
   return (
-    <View>
-      <Text>Crie seu Empreendimento</Text>
-      <Text>Divulgue seu empreendimento no Itapas tur!</Text>
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>Crie seu Empreendimento</Text>
+        <Text style={styles.description}>Divulgue seu empreendimento no Itapas tur!</Text>
+      </View>
 
       <TextInput
         label="Nome"
@@ -49,7 +51,7 @@ const CreateEnterprise = () => {
         color='gray'
         value={enterpriseData.name}
         placeholder="Açaí da Pedra do Frade"
-        style={styles.nameInput}
+        style={styles.input}
       />
 
       <SelectDropdown
@@ -67,8 +69,11 @@ const CreateEnterprise = () => {
         onChangeText={onChangeDescriptionInput}
         color='gray'
         value={enterpriseData.description}
+        multiline
+        numberOfLines={8}
+        textAlignVertical="top"
         placeholder="Venda seu peixe aqui! :)"
-        style={styles.nameInput}
+        style={styles.input}
       />
 
       <TextInput
@@ -79,17 +84,19 @@ const CreateEnterprise = () => {
         color='gray'
         value={enterpriseData.phoneNumber}
         placeholder="+ 55 85 99999-9999"
-        style={styles.nameInput}
+        style={styles.input}
       />
 
-      <Button
-        title={"PRÓXIMA ETAPA"}
-        titleStyle={styles.buttonText}
-        color="#1daf6e"
-        contentContainerStyle={{height: 50}}
-        onPress={() => {}}
-        style={styles.button}
-      />
+      <View style={styles.buttonContainer}>
+        <Button
+          title={"PRÓXIMA ETAPA"}
+          titleStyle={styles.buttonText}
+          color="#1daf6e"
+          contentContainerStyle={{height: 50}}
+          onPress={() => {}}
+          style={styles.button}
+        />
+      </View>
 
       <Text>{enterpriseData.name}</Text>
     </View>
@@ -97,19 +104,45 @@ const CreateEnterprise = () => {
 }
 
 const styles = StyleSheet.create({
-  nameInput: {
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 20
+  },
+  textContainer: {
+    marginBottom: 15,
+  },
+  title: {
+    fontSize: 40,
+    textAlign: "center",
+    fontWeight: "bold",
+    marginBottom: 5
+  },
+  description: {
+    fontSize: 20,
+    textAlign: "center",
+    color: "#999999"
+  },
+  input: {
+    width: 350,
     borderColor: 'gray',
     color: 'gray',
     tintColor: 'gray',
-    marginBottom: 5
+    marginBottom: 10
   },
   selectDropdown: {
+    width: 350,
     borderWidth: 0.8,
     borderColor: "gray",
     borderRadius: 3,
+    marginBottom: 10
   },
   dropdown: {
     borderRadius: 3,
+  },
+  buttonContainer: {
+    marginTop: 50
   },
   button: {
     width: 350,
