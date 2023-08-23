ee1a54fc06f8ba20fa731561eb5c7c4ea7bbe43c
Matthews Bastos <matthewsbasttos@gmail.com>
2023-08-23 14:02:01 -0300

    feat: Implements KeyboardAvoidingWrapper component

---
+++ src/components/KeyboardAvoidingWrapper.jsx
@@ -0,0 +1,20 @@
+import {
+  ScrollView,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard
+} from "react-native";
+
+const KeyboardAvoidingWrapper = ({ children }) => {
+  return (
+    <KeyboardAvoidingView style={{ flex: 1 }}>
+      <ScrollView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          {children}
+        </TouchableWithoutFeedback>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  )
+};
+
+export default KeyboardAvoidingWrapper;
