a5139039743d554a67964a30bb48565c51b69837
Matthews Bastos <matthewsbasttos@gmail.com>
2023-08-24 10:10:29 -0300

    feat: Implements pickImage from gallery

--- src/views/EnterpriseImageCreation.jsx
+++ src/views/EnterpriseImageCreation.jsx
@@ -1,78 +1,96 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, TouchableWithoutFeedback, Image } from "react-native";
 import { Text, Button } from "@react-native-material/core";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
 import { useEnterprise } from "../contexts/EnterpriseContext";
 
 const EnterpriseImageCreation = ({ navigation }) => {
- return (
-  <View style={styles.container}>
-    <View style={styles.content}>
-      <View style={styles.textContainer}>
-        <Text style={styles.title}>Mostre seu empreendimento ao mundo!</Text>
-        <Text style={styles.description}>Esse é o momento de caprichar!</Text>
+  const { enterpriseData, setEnterpriseData } = useEnterprise();
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      const imageURI = result.assets[0].uri;
+
+      setEnterpriseData((prevState) => ({
+        ...prevState,
+        images: [...prevState.images, imageURI],
+      }));
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>Mostre seu empreendimento ao mundo!</Text>
+          <Text style={styles.description}>Esse é o momento de caprichar!</Text>
+        </View>
+
+        {enterpriseData.images[0] ? (
+          <Image source={{ uri: enterpriseData.images[0] }} style={styles.imageContainer}/>
+          ) : (
+            <TouchableWithoutFeedback onPress={pickImage}>
+              <View style={styles.selectImageContainer}>
+                <MaterialCommunityIcons name={ 'camera-plus' } size={50} color={"#B0B0B0"} />
+
+                <View style={styles.plusCircleContainer}>
+                  <MaterialCommunityIcons name={ 'plus-circle' } size={40} color={"#000000"} />
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+          )
+        }
+
+        {enterpriseData.images[1] ? (
+          <Image source={{ uri: enterpriseData.images[1] }} style={styles.imageContainer}/>
+          ) : (
+            <TouchableWithoutFeedback onPress={pickImage}>
+              <View style={styles.selectImageContainer}>
+                <MaterialCommunityIcons name={ 'camera-plus' } size={50} color={"#B0B0B0"} />
+
+                <View style={styles.plusCircleContainer}>
+                  <MaterialCommunityIcons name={ 'plus-circle' } size={40} color={"#000000"} />
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+          )
+        }
+
+        {enterpriseData.images[2] ? (
+          <Image source={{ uri: enterpriseData.images[2] }} style={styles.imageContainer}/>
+          ) : (
+            <TouchableWithoutFeedback onPress={pickImage}>
+              <View style={styles.selectImageContainer}>
+                <MaterialCommunityIcons name={ 'camera-plus' } size={50} color={"#B0B0B0"} />
+
+                <View style={styles.plusCircleContainer}>
+                  <MaterialCommunityIcons name={ 'plus-circle' } size={40} color={"#000000"} />
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+          )
+        }
       </View>
 
-      <View style={styles.selectImageContainer}>
-        <MaterialCommunityIcons
-          name={ 'camera-plus' }
-          size={50}
-          color={"#B0B0B0"}
-        />
-
-        <View style={styles.plusCircleContainer}>
-          <MaterialCommunityIcons
-            name={ 'plus-circle' }
-            size={40}
-            color={"#000000"}
+      <View style={styles.buttonContainer}>
+          <Button
+            title={"FINALIZAR CADASTRO"}
+            titleStyle={styles.buttonText}
+            color="#1daf6e"
+            contentContainerStyle={{height: 50}}
+            onPress={() => {}}
+            style={styles.button}
           />
         </View>
-      </View>
-
-      <View style={styles.selectImageContainer}>
-        <MaterialCommunityIcons
-          name={ 'camera-plus' }
-          size={50}
-          color={"#B0B0B0"}
-        />
-
-        <View style={styles.plusCircleContainer}>
-          <MaterialCommunityIcons
-            name={ 'plus-circle' }
-            size={40}
-            color={"#000000"}
-          />
-        </View>
-      </View>
-
-      <View style={styles.selectImageContainer}>
-        <MaterialCommunityIcons
-          name={ 'camera-plus' }
-          size={50}
-          color={"#B0B0B0"}
-        />
-
-        <View style={styles.plusCircleContainer}>
-          <MaterialCommunityIcons
-            name={ 'plus-circle' }
-            size={40}
-            color={"#000000"}
-          />
-        </View>
-      </View>
     </View>
-
-    <View style={styles.buttonContainer}>
-        <Button
-          title={"FINALIZAR CADASTRO"}
-          titleStyle={styles.buttonText}
-          color="#1daf6e"
-          contentContainerStyle={{height: 50}}
-          onPress={() => {}}
-          style={styles.button}
-        />
-      </View>
-  </View>
- )
+  )
 };
 
 const styles = StyleSheet.create({
@@ -111,6 +129,12 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     borderColor: "#8C8C8C",
     marginBottom: 12
+  },
+  imageContainer: {
+    width: 350,
+    height: 150,
+    borderRadius: 8,
+    marginBottom: 10
   },
   plusCircleContainer: {
     position: "absolute",
